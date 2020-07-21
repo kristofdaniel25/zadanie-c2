b8d165f3a70d2f254530e566a4c644bcb6f47075
Daniel Kristof <danokristof@gmail.com>
2020-07-21 18:02:26 +0200

    gauss alg v0.2

--- main.c
+++ main.c
@@ -54,17 +54,16 @@ char mat_solve_gauss(MAT* a, MAT* b)
 {
 	int i, j;
 	float guide;
+
+	guide = ELEM(a, 0, 0);
 	for (i = 0; i < a->cols; i++)
 	{
-		ELEM(a, 0, i) = ELEM(a, 0, 0) / ELEM(a, 0, i);
+		ELEM(a, 0, i) = ELEM(a, 0, i) / guide;
 	}
-	ELEM(b, 0, 0) = ELEM(a, 0, 0) / ELEM(b, 0, 0);
+	ELEM(b, 0, 0) = ELEM(a, 0, 0) / guide;
 
-	mat_print(a);
-	printf("\n");
-	mat_print(b);
-	printf("\n"); printf("\n");
-
+	unsigned int c = a->cols - 1;
+	printf("c = %d\n", c);
 	for (i = 1; i < a->rows; i++)
 	{
 		guide = -1 * ELEM(a, i, 0);
@@ -72,8 +71,9 @@ char mat_solve_gauss(MAT* a, MAT* b)
 		{
 			ELEM(a, i, j) += ELEM(a, 0, j) * guide;
 		}
-		ELEM(b, i, 1) += ELEM(a, 0, j) * guide;
-	    printf("b_%d: %.3f\n",i, ELEM(b, i, 1));
+		printf("1] b_%d  %.3f\n", i, ELEM(b, i, 0));
+		ELEM(b, i, 0) = ELEM(b, i, 0) + ELEM(a, 0, c) * guide; 
+		printf("2] b_%d  %.3f\n",i, ELEM(b, i, 0));
 	}
 }
 
@@ -97,9 +97,10 @@ int main()
 	mat_random(b);
 	
 	mat_print(a);
-	printf("\n");
 	mat_print(b);
+
 	printf("\n"); printf("\n");
+	
 	mat_solve_gauss(a, b);
 	mat_print(a);
 	mat_print(b);
