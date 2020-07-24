12ba69c2ad5c0ebcd9b7bbdd8540989cf4116129
Daniel Kristof <danokristof@gmail.com>
2020-07-24 15:37:18 +0200

    vytvoreny alg + pridany mat_unit v1.0

--- main.c
+++ main.c
@@ -33,9 +33,20 @@ void mat_random(MAT* mat) {
 	for (i = 0; i < mat->cols; i++) {
 		for (j = 0; j < mat->rows; j++) {
 			ELEM(mat, i, j) = -1 + (1 - (-1)) * ((float)rand() / (float)(RAND_MAX));
+			//ELEM(mat, i, j) = rand() % 15; ---> pre overenie spravnosti vysledku
 		}
 	}
 	
+}
+
+void mat_unit(MAT* mat) {
+	int i, j;
+	for (i = 0; i < mat->rows; i++) {
+		for (j = 0; j < mat->cols; j++) {
+			if (i == j) ELEM(mat, i, j) = 1;
+			else ELEM(mat, i, j) = 0;
+		}
+	}
 }
 
 void mat_print(MAT* mat) {
@@ -50,27 +61,29 @@ void mat_print(MAT* mat) {
 	}
 }
 
-char mat_solve_gauss(MAT* a, MAT* b)
+char mat_solve_gauss(MAT* a, MAT* b, MAT* s)
 {
 	int i, j, k;
 	float guide;
 	unsigned int c = a->cols - 1;
-
-
+	/*
+	CYKLUS NA VYTVORENIE HORNEJ TROJUHOLNIKOVEJ MATICE Z MATICE a:
+	1)predelenie riadku prvym nenulovym elementom riadku (cyklus predelenia)
+	2)scitanie kazdeho elementu pod riadkom s nasobkom daneho riadku (cyklus scitania)
+	Pozn. Oba cykly dodatocne pracuju aj s maticou b. Hodit cely algoritmus do jedneho cyklu by bolo menej narocnejsie na pamat, avsak aj menej prehladne.		
+	*/
+	
 	for (k = 0; k < a->rows; k++)
 	{
 		guide = ELEM(a, k, k);
-		ELEM(b, k, 0) = ELEM(b, k, 0) / guide;
-		
-        for (i = k; i < a->cols; i++)
+	
+        for (i = k; i < a->cols; i++)	//cyklus predelenia
 		{
 			ELEM(a, k, i) = ELEM(a, k, i) / guide;
 		}
+		ELEM(b, k, 0) = ELEM(b, k, 0) / guide;
 		
-
-		printf("c = %d\n", c);
-
-		for (i = k+1; i < a->rows; i++)
+		for (i = k+1; i < a->rows; i++)	//cyklus scitania
 		{
 			guide = -1 * ELEM(a, i, k);
 			for (j = k; j < a->cols; j++)
@@ -78,19 +91,41 @@ char mat_solve_gauss(MAT* a, MAT* b)
 				ELEM(a, i, j) += ELEM(a, k, j) * guide;
 			}
 
-			ELEM(b, i, 0) += ELEM(a, 0, c) * guide;
+			ELEM(b, i, 0) += ELEM(b, k, 0) * guide;
 		}
 	}
+	
+	for (k = a->rows -1; k > -1; k--)
+	{
+		
+		for (i = k - 1; i > - 1; i--)	//cyklus scitania
+		{
+			guide = -1 * ELEM(a, i, k);
+			for (j = k; j < a->cols; j++)
+			{
+				ELEM(a, i, j) += ELEM(a, k, j) * guide;
+			}
+
+			ELEM(b, i, 0) += ELEM(b, k, 0) * guide;
+		}
+	}
+	
+	for (i = 0; i < b->rows; i++)
+	{
+		ELEM(s, i, 0) = ELEM(b, i, 0);
+	}
+
 }
 
 
 void mat_destroy(MAT* mat) {
 	free(mat);
+	free(mat->elem);
 }
 
 int main()
 {
-	MAT* a, * b;
+	MAT* a, * b, *s;
 	unsigned int n;
 	srand(time(NULL));
 
@@ -99,19 +134,20 @@ int main()
 
 	a = mat_create_with_type(n, n);
 	b = mat_create_with_type(n, 1);
-	
+	s = mat_create_with_type(n, 1);
+
 	mat_random(a);
 	mat_random(b);
 	
-	mat_print(a);
-	mat_print(b);
 
 	printf("\n"); printf("\n");
 	
-	mat_solve_gauss(a, b);
-	mat_print(a);
-	mat_print(b);
+	mat_solve_gauss(a, b, s);
+
+	mat_print(s);
+
 	mat_destroy(a);
 	mat_destroy(b);
+	mat_destroy(s);
 	return 0;
 }
